eb73f58614862e5b585d02fa1ce9bf0a4f9984a9
ALVARO ADRIAN IZQUIERDO GOMEZ <aizquierdo@itp.edu.co>
2022-10-26 19:34:01 -0500

    agregando archivos

---
+++ registry-eureka/src/test/java/itp/instituto/registry/RegistryEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package itp.instituto.registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistryEurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
